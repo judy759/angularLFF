3ae8e06658ffb9c8d2f21214c37ba628fe053a43
Shira72 <sba3145572@gmail.com>
2024-12-21 22:03:24 +0200

    video 5-7

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { core } from "@angular/compiler";
 import {Component} from "@angular/core";
+import { StudentListComponent } from "./student-list/student-list.component";
 
 
 
 @Component({
     //template:"<h1>hello this is my project<h1>",
     templateUrl:"app.component.html",
-    selector:"app-root"
+    selector:"app-root",
+    imports:[StudentListComponent]
+    
   })
 
   
