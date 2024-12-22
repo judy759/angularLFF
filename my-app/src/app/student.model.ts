990fc405192b5d3de32adfc0936159f9821f19fb
Shira72 <sba3145572@gmail.com>
2024-12-22 23:50:11 +0200

    video 8-10

--- my-app/src/app/student.model.ts
+++ my-app/src/app/student.model.ts
@@ -2,8 +2,9 @@
 export class Student{
     id: number;
     name: string;
+    grade?:number;
     
-    constructor(id: number, name:string){
+    constructor(id:number,name:string){
         this.id=id;
         this.name=name;
     }
